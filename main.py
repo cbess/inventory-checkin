05c81137bf9f1d9b4d74767ddf4dd449cdf2a76f
C. Bess <cbess@quantumquinn.com>
2012-09-27 17:29:21 -0500

    updated ignore, added setup files

---
+++ main.py
@@ -0,0 +1,82 @@
+#! python
+# encoding: utf-8
+""" 
+main.py
+Created by Christopher Bess (https://github.com/cbess/inventory-checkin)
+Copyright 2012
+"""
+script_description = 'Runs the main application.'
+try:
+    # optparse is deprecated, but I wanted broader compatibility
+    from optparse import OptionParser
+    parser = OptionParser(description=script_description)
+    add_argument = parser.add_option
+except ImportError:
+    # this is here to help any future upgrades
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=script_description)
+    add_argument = parser.add_argument
+    pass
+
+from webapp import server
+import tests
+import settings
+import os
+import sys
+from datetime import datetime
+
+
+def get_app_args():
+    """Returns the application arguments from stdin
+    @return Object optparse.Values or argparse.Namespace
+    """
+    arguments = parser.parse_args()
+    if isinstance(arguments, tuple):
+        # assume its optparse return value
+        (opts, args) = arguments
+        return opts
+    return arguments
+    
+
+def get_options():
+    """ Returns the options from the script """
+    add_argument("--test", dest="run_tests",
+                    action='store_true',
+                      help="Run tests to ensure everything works correctly.")
+    add_argument('--runserver', dest='run_server',
+                 action='store_true',
+                    help='Run the Sherlock web server.')
+    add_argument('-v', '--version', dest='show_version',
+                 action='store_true',
+                    help='Show sherlock version information.')
+    # not available, yet
+#    add_argument("-q", "--quiet",
+#                      action="store_false", dest="verbose", default=True,
+#                      help="Don't print status messages to stdout.")
+    options = get_app_args()
+    return options
+    
+    
+def run():
+    options = get_options()
+    # determine app action
+    if options.run_tests:
+        tests.run_all()
+    elif options.show_version:
+        pyver = sys.version_info
+        print '  Python: v%d.%d.%d' % (pyver.major, pyver.minor, pyver.micro)
+        print '   Flask: v' + get_version_info('flask')
+        print 'CherryPy: v' + get_version_info('cherrypy')
+    elif options.run_server:
+        print 'Server: %s' % settings.SERVER_TYPE
+        # launch web server
+        server.run()
+    else:
+        print 'Use -h to see options.'
+    pass
+    
+
+if __name__ == '__main__':
+    print 'inventory checkin v%s started' % get_version_info('ici')
+    run()
+    print 'inventory checkin done.'
---
+++ setup/virtualenv-setup.sh
@@ -0,0 +1,31 @@
+# global
+# Created by: Christopher Bess
+# It is recomm. not to run this script using sudo, it is used as needed.
+#
+# Run this script from this `setup` directory.
+#
+# user$: sh ./virtualenv-setup.sh
+
+VENV_NAME=inventorycheckin_env
+
+# echo "Setting up virtualenv and pip"
+# sudo easy_install pip
+# sudo pip install virtualenv
+
+# adjust permission (allow it to be executed)
+chmod +x ../main.py
+
+# if on a Mac exec below line (maybe)
+# ARCHFLAGS="-arch i386 -arch x86_64"
+
+# setup sherlock environment
+virtualenv ../$VENV_NAME --distribute --no-site-packages
+
+echo "Installing dependencies"
+../$VENV_NAME/bin/pip install -r requirements.txt
+
+# confirm installation by showing version information
+# echo "Sherlock version information"
+# ../$VENV_NAME/bin/python ../main.py -v
+
+echo "Done, install finished"
