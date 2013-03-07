0a8799924f96f513005e0c55af16570d0f3885e1
Christopher Bess <christopher.bess@bottlerocketapps.com>
2013-03-07 13:05:45 -0600

    - update root dir constant

--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ DEBUG = True
 # Should not be changed, this is the absolute path to the directory
 # containing main.py, core/, etc.
 # type: string
-# default: os.path.abspath('.')
-ROOT_DIR = os.path.abspath('.')
+# default: os.path.dirname(__file__)
+ROOT_DIR = os.path.dirname(__file__)
 
 # An absolute path to the directory path that will store the logs.
 # Set to an empty string to disable logging.
